1d981fcd46c81a72036b60f2cfa3d211ff0ce4e0
Anastasia <79032456178@yandex.ru>
2022-09-01 01:02:37 +0300

    Добавьте файлы проекта.

---
+++ PythonApplication2.py
@@ -0,0 +1,82 @@
+# типы данных и переменная
+# int, float, boolen, str, list, None
+# print('hello world')
+
+# value = None
+# value = 12334
+# print(type(value))
+# print(type(a))
+# print(type(b))
+# s = 'hello world'
+
+# a = 123
+# b = 1.23
+
+# print(s) # вывод строки
+
+# print(a, '-',b, '-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+# list = ['1', '2', '3', 'hello']
+# print(list)
+
+# Ввод и вывод данных
+# print input
+
+# print('input a');
+# a = float(input())
+# print('input b');
+# b = float(input())
+# print(a, ' + ' , b, ' = ', a+b)
+# print('{} {}'.format(a, b))
+# print(f'{a}, {b}')
+
+# Арифметические операции
+# +,-,*,/,%,//,**
+# **,+,-,*,/,//,%,+,-
+# (), Сокращенные операции
+# a = 123
+# b = -321
+# c = a+b
+# print(c)
+
+# a = 3
+# b = 5
+# a+=5
+# print(a)
+
+# Логические операции
+# >,>=, <, <=, ==, !=
+# not, and, or - не путать с &, |, ^
+# is, is not, in, not in
+# gen
+
+# f = 1 > 2 or 4 < 6
+
+# print(f)
+
+# Управляющие конструкции
+# if, if-else
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+    # print(a)
+
+# else:
+        # print(b)
+        # Не запустилось
+        username  = input('input name: ')
+        if username == 'Maria':
+            print('Yeah, it's MARIA!')
+        elif username == 'Marina':
+            print('I was waiting for you Marina so much!')
+        elif username == 'Ilnar':
+            print('Ilnar is top)')
+        else:
+            print('Hi, ', username')
+
+
